68ce78a50c98060eefe1aea90c44d5c3f6896868
jwowillo <will.orourke10@gmail.com>
2025-03-16 17:08:40 +0100

    Update Infra

--- generate/terraform/file.go
+++ generate/terraform/file.go
@@ -181,6 +181,20 @@ provider "google" {
 	region = "us-east1"
 	zone = "us-east1-b"
 }
+
+resource "random_id" "bucket_prefix" {
+	byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+	name = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+	force_destroy = false
+	location = "US"
+	storage_class = "STANDARD"
+	versioning {
+		enabled = true
+	}
+}
 `
 
 const jobTriggerTemplate = `
